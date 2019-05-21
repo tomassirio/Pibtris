41166ce9bc78a4e209eacaf2c59317aa49756648
tomassirio <tomas.sirio@leniolabs.com>
2019-05-21 16:34:55 -0300

    Tetris finalized

---
+++ Assets/SpawnTetromino.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTetromino : MonoBehaviour {
+
+    public GameObject[] Tetrominoes;
+
+	// Use this for initialization
+	void Start () {
+        NewTetromino();
+	}
+	
+	public void NewTetromino() {
+        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
+	}
+}
--- Assets/TetrisBlock.cs
+++ Assets/TetrisBlock.cs
@@ -11,6 +11,8 @@ public class TetrisBlock : MonoBehaviour {
     public static int height = 20;
     public static int width = 10;
 
+    private static Transform[,] grid = new Transform[width, height];
+
 	// Use this for initialization
 	void Start () {
 		
@@ -44,13 +46,77 @@ public class TetrisBlock : MonoBehaviour {
         }
             
 
-        if (Time.time - previousTime > (Input.GetKeyDown(KeyCode.DownArrow) ? fallTime / 10 : fallTime)) {
+        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime)) {
             transform.position += new Vector3(0, -1, 0);
             if (!ValidMove())
             {
                 transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                CheckForLines();
+
+                this.enabled = false;
+                FindObjectOfType<SpawnTetromino>().NewTetromino();
             }
             previousTime = Time.time;
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;
+        }
+    }
+
+    void CheckForLines()
+    {
+        for (int i -height - 1; i > 0; i--){
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    bool HasLine(int i)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            if (grid[j, i] == null)
+                return false;
+        }
+        return true;
+    }
+
+    void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+
+        }
+        
+    }
+
+    void RowDown(int i)
+    {
+        for(int j = i; j < height; j++)
+        {
+            for(int k = 0; k < width; k++)
+            {
+                if(grid[k,j] != null)
+                {
+                    grid[k, j - 1] = grid[k, j];
+                    grid[k, j] = null;
+                    grid[k, j - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
         }
     }
 
@@ -62,6 +128,9 @@ public class TetrisBlock : MonoBehaviour {
             int roundedY = Mathf.RoundToInt(children.transform.position.y);
 
             if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+                return false;
+
+            if (grid[roundedX, roundedY] != null)
                 return false;
         }
         return true;
